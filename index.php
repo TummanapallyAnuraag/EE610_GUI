3796d279dba4011d50108734e1850945004b5659
TummanapallyAnuraag <tummanapally.anuraag@gmail.com>
2018-08-18 22:41:59 +0530

    Buttons aligned - Desktop

--- index.php
+++ index.php
@@ -28,69 +28,107 @@
 	<!-- The control panel of my GUI -->
 	<controls>
 		<div>
+			<div class="control-box">
 			<!-- Upload Image -->
-			<form id="image_form" method="POST" action="upload.php" enctype="multipart/form-data">
-				<button type="button" class="btn btn-default btn-md" id="upload_button">
-					<span class="glyphicon glyphicon-upload"></span> Upload
+			<div class="control-row">
+				<div class="col-sm-6">
+				<form id="image_form" method="POST" action="upload.php" enctype="multipart/form-data">
+					<button type="button" class="btn btn-default btn-md" id="upload_button">
+						<span class="glyphicon glyphicon-upload"></span> Upload
+					</button>
+					<input id="upload_input" type="file" onchange="customSubmit(this.form)" name="pic" accept="image/*" style="display:none;">
+					<!-- <input type="submit" name="submit" value="submit"> -->
+				</form>
+				</div>
+				<!-- Download Image -->
+				<div class="col-sm-6">
+					<a id="target_slave" href="images/_gui/default.jpg" download>
+					<button type="button" class="btn btn-default btn-md">
+						<span class="glyphicon glyphicon-save"></span> Save
+					</button>
+				</a>
+				</div>
+			</div>
+			<div class="control-row">
+				<!-- Zoom In -->
+				<div class="col-sm-6">
+					<button type="button" class="btn btn-default btn-md" id="zoom_in">
+					<span class="glyphicon glyphicon-zoom-in"></span> Max
 				</button>
-				<input id="upload_input" type="file" onchange="customSubmit(this.form)" name="pic" accept="image/*" style="display:none;">
-				<!-- <input type="submit" name="submit" value="submit"> -->
-			</form>
-			<!-- Zoom In -->
-			<button type="button" class="btn btn-default btn-md" id="zoom_in">
-				<span class="glyphicon glyphicon-zoom-in"></span>
-			</button>
-			<!-- Zoom Out -->
-			<button type="button" class="btn btn-default btn-md" id="zoom_out">
-				<span class="glyphicon glyphicon-zoom-out"></span>
-			</button>
+				</div>
+				<!-- Zoom Out -->
+				<div class="col-sm-6">
+					<button type="button" class="btn btn-default btn-md" id="zoom_out">
+					<span class="glyphicon glyphicon-zoom-out"></span> Min
+				</button>
+				</div>
+			</div>
+			<div class="control-row">
+				<!-- Undo -->
+				<div class="col-sm-6">
+					<button type="button" class="btn btn-default btn-md" id="undo">
+					<span style="transform: rotateY(180deg);" class="glyphicon glyphicon-repeat"></span> Undo
+				</button>
+				</div>
+				<!-- Reset -->
+				<div class="col-sm-6">
+					<button type="button" class="btn btn-default btn-md" id="reset">
+					<span class="glyphicon glyphicon-refresh"></span> Reset
+				</button>
+				</div>
+			</div>
+			<!-- Toggle View - Original/Recent Image -->
+			<div class="control-row-single" >
+					<button type="button" class="btn btn-default btn-md" id="targetChange" show="0">
+					<span class="glyphicon glyphicon-eye-open"></span> View <span id="text">Original</span>
+					</button>
+			</div>
+			</div>
+			<div class="control-box">
 			<!-- Histogram Equalisation -->
-			<button type="button" class="btn btn-default btn-md" id="histeq">
+			<div class="control-row-single">
+				<button type="button" class="btn btn-default btn-md" id="histeq">
 				Histogram Equalise
-			</button><br>
-			<!-- Download Image -->
-			<a id="target_slave" href="images/_gui/default.jpg" download>
-				<button type="button" class="btn btn-default btn-md">
-					<span class="glyphicon glyphicon-save"></span> Save
-				</button>
-			</a><br>
+				</button><br>
+			</div>
 			<!-- Log Transform -->
-			<button type="button" class="btn btn-default btn-md" id="logtx">
+			<div class="control-row-single">
+				<button type="button" class="btn btn-default btn-md" id="logtx">
 				Log Transform
 			</button><br>
+			</div>
 			<!-- Gamma Correction -->
-			<div id="gamma_correct_frame">
-				<input placeholder="gamma" value="" name="gamma" class="form-control" style="width:200px;">
-				<button type="button" class="btn btn-default btn-md" id="gamma_correct">
-					Gamma Correct
-				</button>
+			<div class="control-row">
+				<div id="gamma_correct_frame">
+					<div class="col-sm-6">
+					<input placeholder="gamma" value="" name="gamma" class="form-control" style="width:120%;">
+					</div>
+					<div class="col-sm-6">
+					<button type="button" class="btn btn-default btn-md" id="gamma_correct">
+						<b>&gamma;</b> Correct
+					</button>
+					</div>
+				</div>
 			</div>
 			<!-- Blurring -->
-			<div id="blur_frame">
-				<input id="blur_range" type="range" min="1" max="30" value="15">
-				<button type="button" class="btn btn-default btn-md" id="blur">
-					Blur
-				</button>
+			<div class="control-row-single" style="margin: 10px 0px;">
+				<div id="blur_frame">
+					<input id="blur_range" class="form-control" type="range" min="1" max="30" value="15">
+					<button type="button" class="btn btn-default btn-md" id="blur">
+						Blur
+					</button>
+				</div>
 			</div>
 			<!-- Sharpening -->
-			<div id="sharp_frame">
-				<input id="sharp_range" divideby="100" type="range" min="1" max="50" value="10">
+			<div class="control-row-single">
+				<div id="sharp_frame">
+				<input id="sharp_range" class="form-control" divideby="100" type="range" min="1" max="50" value="10">
 				<button type="button" class="btn btn-default btn-md" id="sharp">
 					Sharp
 				</button>
 			</div>
-			<!-- Undo -->
-			<button type="button" class="btn btn-default btn-md" id="undo">
-				<span style="transform: rotateY(180deg);" class="glyphicon glyphicon-repeat"></span> Undo
-			</button><br>
-			<!-- Reset -->
-			<button type="button" class="btn btn-default btn-md" id="reset">
-				<span class="glyphicon glyphicon-refresh"></span> Reset
-			</button><br>
-			<!-- Toggle View - Original/Recent Image -->
-			<button type="button" class="btn btn-default btn-md" id="targetChange" show="0">
-				<span class="glyphicon glyphicon-eye-open"></span> View <span id="text">Original</span>
-			</button><br>
+			</div>
+			</div>
 		</div>
 	</controls>
 	<!-- Control Panel Ends Here -->
